4ed0df8b0ef975355d5f54992cfecfecfa80b878
wenqiang.jiang <wenqiang.jiang@szagv.com>
2020-03-08 15:21:17 +0800

    first blood 20200308

---
+++ DotnetHelper/DotnetHelper/ObjectExt.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetHelper
+{
+    public static class ObjectHelper
+    {
+        private static JsonSerializerSettings SerializerSettings = new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.All, DateFormatHandling = DateFormatHandling.IsoDateFormat, DateTimeZoneHandling = DateTimeZoneHandling.Utc };
+        public static T DeepCopy<T>(this object obj)
+        {
+            string str = JsonConvert.SerializeObject(obj, SerializerSettings);
+            T result = JsonConvert.DeserializeObject<T>(str);
+            return result;
+        }
+    }
+}
---
+++ DotnetHelper/DotnetHelperTests/ObjectExtTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotnetHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetHelper.Tests
+{
+    [TestClass()]
+    public class ObjectExtTests
+    {
+        [TestMethod()]
+        public void DeepCopyTest()
+        {
+
+            var a = new ABC();
+            var b=a.DeepCopy<ABC>();
+            a.A = "FFF";
+
+            Assert.Fail();
+        }
+        public class ABC
+        {
+            public string A { get; set; } = string.Empty;
+        }
+    }
+}
